7f1c8e05182d0ad997a13377ce1bbfe10ee2085a
karolinasmile@mail.ru <karolinasmile@mail.ru>
2022-10-05 17:12:28 +0300

    HW5Commit

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import {Route, Redirect, Switch} from 'react-router-dom'
 import Error404 from './pages/Error404'
 import PreJunior from './pages/PreJunior'
 import Junior from './pages/Junior'
@@ -15,7 +15,12 @@ function Pages() {
     return (
         <div>
             {/*Routes выбирает первый подходящий роут*/}
-            <Routes>
+            <Switch>
+                <Route path={'/'}  render={()=><Redirect to={PATH.PRE_JUNIOR}/>}/>
+                <Route path={PATH.PRE_JUNIOR} render={()=> <PreJunior/>} />
+                <Route path={PATH.JUNIOR} render={()=> <Junior/>} />
+                <Route path={PATH.JUNIOR_PLUS} render={()=> <JuniorPlus/>} />
+                <Route render={()=><Error404/>}/>
                 {/*роутинг будут писать студенты*/}
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
                 {/*<Route ...*/}
@@ -27,7 +32,7 @@ function Pages() {
 
                 {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
                 {/*<Route ...*/}
-            </Routes>
+            </Switch>
         </div>
     )
 }
