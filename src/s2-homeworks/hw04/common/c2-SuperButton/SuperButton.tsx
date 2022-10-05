edbbd540655e6ef0a09e21b70a01f760d2dcbc82
karolinasmile@mail.ru <karolinasmile@mail.ru>
2022-10-05 11:41:21 +0300

    HW4Commit

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -27,7 +27,7 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         //         ? ...
         //         : xType === 'red'
         //             ? ...
-     // + (className ? ' ' + className : '') // задачка на смешивание классов
+      + (className ? ' ' + className : '') // задачка на смешивание классов
 
     return (
         <button
